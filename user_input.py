136e4991a70442fb9ea799b5ea5b20ec587a8726
Lillian Nyenge <lillianmukii@gmail.com>
2024-03-05 22:01:27 +0300

    Python assignment

---
+++ user_input.py
@@ -0,0 +1,15 @@
+"user_input.py"
+
+# Ask the user for their name and store it in a variable
+name = input("What is your name ?")
+
+#Ask the user for their age and store it in a variable
+age = input("How old are you?")
+
+#Ask the user for their location and store it in a variable
+location = input("Where do you live?")
+
+# Print out a personalized message using the user's information
+print("Hello " +name+ " you are " +age+ " years old and live in " +location)
+
+
